c34a33c31bada4c8d827fff4b3e02fc102f503c0
James Vanderhyde <vanderhyde@sxu.edu>
2016-08-05 21:52:07 -0500

    Updated leiningen project info

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject wtrts "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :description "The World's Tiniest Real-Time Strategy Game"
+  :url "https://github.com/jvanderhyde/wtrts"
+  :license {:name "The MIT License"
+            :url "https://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [quil "2.4.0"]])
